a6b5462f212fa24885fc4928b59cfe6c735d1ddc
James Thorniley <james.thorniley@mixcloud.com>
2024-01-03 11:22:05 +0000

    Do not allow response.close to be cancelled in asgi handler

--- django/core/handlers/asgi.py
+++ django/core/handlers/asgi.py
@@ -296,57 +296,65 @@ class ASGIHandler(base.BaseHandler):
         """Encode and send a response out over ASGI."""
         # Collect cookies into headers. Have to preserve header case as there
         # are some non-RFC compliant clients that require e.g. Content-Type.
-        response_headers = []
-        for header, value in response.items():
-            if isinstance(header, str):
-                header = header.encode("ascii")
-            if isinstance(value, str):
-                value = value.encode("latin1")
-            response_headers.append((bytes(header), bytes(value)))
-        for c in response.cookies.values():
-            response_headers.append(
-                (b"Set-Cookie", c.output(header="").encode("ascii").strip())
-            )
-        # Initial response message.
-        await send(
-            {
-                "type": "http.response.start",
-                "status": response.status_code,
-                "headers": response_headers,
-            }
-        )
-        # Streaming responses need to be pinned to their iterator.
-        if response.streaming:
-            # - Consume via `__aiter__` and not `streaming_content` directly, to
-            #   allow mapping of a sync iterator.
-            # - Use aclosing() when consuming aiter.
-            #   See https://github.com/python/cpython/commit/6e8dcda
-            async with aclosing(aiter(response)) as content:
-                async for part in content:
-                    for chunk, _ in self.chunk_bytes(part):
-                        await send(
-                            {
-                                "type": "http.response.body",
-                                "body": chunk,
-                                # Ignore "more" as there may be more parts; instead,
-                                # use an empty final closing message with False.
-                                "more_body": True,
-                            }
-                        )
-            # Final closing message.
-            await send({"type": "http.response.body"})
-        # Other responses just need chunking.
-        else:
-            # Yield chunks of response.
-            for chunk, last in self.chunk_bytes(response.content):
-                await send(
-                    {
-                        "type": "http.response.body",
-                        "body": chunk,
-                        "more_body": not last,
-                    }
+        try:
+            response_headers = []
+            for header, value in response.items():
+                if isinstance(header, str):
+                    header = header.encode("ascii")
+                if isinstance(value, str):
+                    value = value.encode("latin1")
+                response_headers.append((bytes(header), bytes(value)))
+            for c in response.cookies.values():
+                response_headers.append(
+                    (b"Set-Cookie", c.output(header="").encode("ascii").strip())
                 )
-        await sync_to_async(response.close, thread_sensitive=True)()
+            # Initial response message.
+            await send(
+                {
+                    "type": "http.response.start",
+                    "status": response.status_code,
+                    "headers": response_headers,
+                }
+            )
+            # Streaming responses need to be pinned to their iterator.
+            if response.streaming:
+                # - Consume via `__aiter__` and not `streaming_content` directly, to
+                #   allow mapping of a sync iterator.
+                # - Use aclosing() when consuming aiter.
+                #   See https://github.com/python/cpython/commit/6e8dcda
+                async with aclosing(aiter(response)) as content:
+                    async for part in content:
+                        for chunk, _ in self.chunk_bytes(part):
+                            await send(
+                                {
+                                    "type": "http.response.body",
+                                    "body": chunk,
+                                    # Ignore "more" as there may be more parts; instead,
+                                    # use an empty final closing message with False.
+                                    "more_body": True,
+                                }
+                            )
+                # Final closing message.
+                await send({"type": "http.response.body"})
+            # Other responses just need chunking.
+            else:
+                # Yield chunks of response.
+                for chunk, last in self.chunk_bytes(response.content):
+                    await send(
+                        {
+                            "type": "http.response.body",
+                            "body": chunk,
+                            "more_body": not last,
+                        }
+                    )
+        finally:
+            # do not allow the response.close callback to be cancelled, as we depend on
+            # handlers triggered from here (in sync code) to clean up resources such as
+            # DB connections
+            await asyncio.shield(
+sync_to_async(response.close, thread_sensitive=True)()
+
+            )
 
     @classmethod
     def chunk_bytes(cls, data):
